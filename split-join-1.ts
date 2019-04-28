38bf1228d3dd31a74e170ee82a26a8fddbc02aa0
Josh Wulf <josh@magikcraft.io>
2019-04-29 09:04:36 +1000

    Give workers unique ids

--- split-join-1.ts
+++ split-join-1.ts
@@ -22,21 +22,30 @@ async function deployWorkflow(bpmnFile: string) {
 
 function createWorkers() {
   const worker = zbc.createWorker(
-    "test",
+    "config-to-payload-worker",
     "config-to-payload",
-   
+
     (job, complete) => {
-      const taskName = job.jobHeaders.elementId
-      console.log('%s" input: %j output: %j', taskName, job.variables, job.customHeaders)
-      complete(job.customHeaders)
+      const taskName = job.jobHeaders.elementId;
+      console.log(
+        '%s" input: %j output: %j',
+        taskName,
+        job.variables,
+        job.customHeaders
+      );
+      complete(job.customHeaders);
     }
   );
 
-  const outputWorker = zbc.createWorker("test", "output", (job, complete) => {
-    const taskName = job.jobHeaders.elementId
-    console.log('%s: %j', taskName, job.variables)
-    complete(job.variables)
-  });
+  const outputWorker = zbc.createWorker(
+    "output-worker",
+    "output",
+    (job, complete) => {
+      const taskName = job.jobHeaders.elementId;
+      console.log("%s: %j", taskName, job.variables);
+      complete(job.variables);
+    }
+  );
 
   return { worker, outputWorker };
 }
--- split-join-3.ts
+++ split-join-3.ts
@@ -6,7 +6,7 @@ test("./bpmn/test-split-join-3.bpmn");
 export async function test(bpmnFile: string) {
   const processId = await deployWorkflow(bpmnFile);
   if (createWorkers()) {
-    await zbc.createWorkflowInstance(processId, {local: true});
+    await zbc.createWorkflowInstance(processId, { local: true });
   }
 }
 
@@ -22,21 +22,30 @@ async function deployWorkflow(bpmnFile: string) {
 
 function createWorkers() {
   const worker = zbc.createWorker(
-    "test",
+    "config-to-payload-worker",
     "config-to-payload",
-   
+
     (job, complete) => {
-      const taskName = job.jobHeaders.elementId
-      console.log('%s" input: %j output: %j', taskName, job.variables, job.customHeaders)
-      complete(job.customHeaders)
+      const taskName = job.jobHeaders.elementId;
+      console.log(
+        '%s" input: %j output: %j',
+        taskName,
+        job.variables,
+        job.customHeaders
+      );
+      complete(job.customHeaders);
     }
   );
 
-  const outputWorker = zbc.createWorker("test", "output", (job, complete) => {
-    const taskName = job.jobHeaders.elementId
-    console.log('%s: %j', taskName, job.variables)
-    complete(job.variables)
-  });
+  const outputWorker = zbc.createWorker(
+    "output-worker",
+    "output",
+    (job, complete) => {
+      const taskName = job.jobHeaders.elementId;
+      console.log("%s: %j", taskName, job.variables);
+      complete(job.variables);
+    }
+  );
 
   return { worker, outputWorker };
 }
